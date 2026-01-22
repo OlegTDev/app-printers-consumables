926664b56308c6585a0010491ef4e5410c03cb03
toatall <_alexeevich_@list.ru>
2026-01-22 16:59:13 +0000

    Созданы shared компоненты Vue (Author, PrinterWorkplace, Timestamps) в частности для использования в таблицах (DataTable)

---
+++ resources/js/Shared/DataTable/Author.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+    login: String,
+    fullName: String,
+    post: String,
+    department: String,
+})
+</script>
+<template>
+    <div>
+        {{ login }}
+    </div>
+    <div v-if="fullName">
+        {{ fullName }}
+    </div>
+    <div v-if="post || department">
+        {{ post }} - {{ department }}
+    </div>
+</template>
---
+++ resources/js/Shared/DataTable/Timestamps.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { inject } from 'vue';
+
+defineProps({
+    created_at: String,
+    updated_at: String,
+})
+
+const moment = inject('moment');
+</script>
+<template>
+    <div class="grid grid-rows-2 gap-2">
+        <div v-if="created_at" v-tooltip="`Создано: ${moment(created_at).format('LLLL')}`">
+            <i class="far fa-calendar"></i>
+            {{ moment(created_at).fromNow() }}
+        </div>                    
+        <div v-if="updated_at && created_at != updated_at" v-tooltip="`Изменено: ${moment(updated_at).format('LLLL')}`">
+            <i class="far fa-calendar-alt"></i>      
+            {{ moment(updated_at).fromNow() }}
+        </div>
+    </div>
+</template>
