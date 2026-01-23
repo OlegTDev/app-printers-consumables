934c5731a6c17b2c42f815616dbee9cc58e89ce0
toatall <_alexeevich_@list.ru>
2026-01-23 14:44:54 +0000

    Вынесены labels в конфигурационные файлы

---
+++ config/labels/order_spare_part.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'id' => 'ИД',
+    'id_order' => 'Заказ',
+    'id_printers_workplace' => 'Принтер',
+    'id_spare_part' => 'Запчасть',
+    'call_specialist' => 'Необходимость вызова специалиста',
+
+    'files' => 'Акт технического состояния',
+    
+    'id_spare_part_or_call_specialist' => 'Услуга или запчасть',
+];
