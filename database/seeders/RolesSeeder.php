4252ec00b5e17579364873299e23dfb6d83c9f31
toatall <_alexeevich_@list.ru>
2026-01-26 16:24:58 +0000

    Добавлена роль order-approver

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -10,48 +10,42 @@ use App\Models\Auth\Role;
 class RolesSeeder extends AbstractSeeder
 {
 
+    private $roles = [
+        // полные права
+        ['admin' => 'Администратор (по всему контексту)'],
+
+        // права для работы со справочниками (управление справочниками принтеров, расходных материалов)
+        ['editor-dictionary' => 'Редактор справочников (принтеров, расходных материалов)'],
+
+        // права на добавление принтеров на местах
+        ['editor-printer-workplace' => 'Редактор принтеров на местах'],
+
+        // права на добавление расходных материалов (количества)
+        ['add-consumables' => 'Добавление расходных материалов'],
+
+        // права на вычитание расходного материала (количества)
+        ['subtract-consumable' => 'Вычитание расходных материалов'],
+
+        // права на согласование заказов
+        ['order-approver' => 'Согласование заказов'],
+    ];
+
     /**
      * Run the database seeds.
      * @return void
      */
     public function run(): void
     {
-        Role::query()->insert([
-            // полные права (управление пользователями, ролями, справочником организаций)
-            [
-                'name' => 'admin', 
-                'description' => 'Администратор (по всему контексту)', 
+        $inserts = [];
+        foreach ($this->roles as $role => $description) {
+            $inserts[] = [
+                'name' => $role,
+                'description'=> $description,
                 'created_at' => $this->getDbNowDate(), 
                 'updated_at' => $this->getDbNowDate(),
-            ],            
-            // права для работы со справочниками (управление справочниками принтеров, расходных материалов)
-            [
-                'name' => 'editor-dictionary', 
-                'description' => 'Редактор справочников (принтеров, расходных материалов)', 
-                'created_at' => $this->getDbNowDate(), 
-                'updated_at' => $this->getDbNowDate(),
-            ],
-            // права на добавление принтеров на местах
-            [
-                'name' => 'editor-printer-workplace', 
-                'description' => 'Редактор принтеров на местах',
-                'created_at' => $this->getDbNowDate(),
-                'updated_at' => $this->getDbNowDate(),
-            ],
-            // права на добавление расходных материалов (количества)
-            [
-                'name' => 'add-consumables', 
-                'description' => 'Добавление расходных материалов',
-                'created_at' => $this->getDbNowDate(), 
-                'updated_at' => $this->getDbNowDate(),
-            ],
-            // права на вычитание расходного материала (количества)
-            [
-                'name' => 'subtract-consumable', 
-                'description' => 'Вычитание расходных материалов',
-                'created_at' => $this->getDbNowDate(), 
-                'updated_at' => $this->getDbNowDate(),
-            ],
-        ]);
+            ];
+        }
+
+        Role::query()->insert($inserts);
     }
 }
