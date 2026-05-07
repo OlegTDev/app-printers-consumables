6e04161cb48b29037b475365902b76cf48b63a42
OlegTDev <_alexeevich_@list.ru>
2026-05-07 17:51:50 +0500

    Отрефакторины общие компоненты по eslint

--- resources/js/Shared/Label.vue
+++ resources/js/Shared/Label.vue
@@ -1,17 +1,11 @@
 <script setup>
-import { useAttrs } from 'vue'
-
-useAttrs()
 const props = defineProps({
     forId: String,
-})
-
+});
 </script>
 
-
-<template>    
-    <label class="block leading-6 text-gray-500 font-bold" :for="forId">
-        <slot />
-    </label>        
+<template>
+  <label class="block leading-6 text-gray-500 font-bold" :for="forId">
+    <slot />
+  </label>
 </template>
-
--- resources/js/Shared/TrashedMessage.vue
+++ resources/js/Shared/TrashedMessage.vue
@@ -1,17 +1,25 @@
 <script setup>
-import Button from 'primevue/button'
+import Button from 'primevue/button';
 
-const emits = defineEmits(['restore'])
+defineEmits(['restore']);
 </script>
 
 <template>
-    <div class="flex items-center justify-between p-4 max-w-full bg-yellow-400 rounded">
-        <div class="flex items-center">
-            <i class="fas fa-trash mr-2 font-medium text-yellow-800"></i>
-            <div class="text-yellow-800 font-medium">
-                <slot />
-            </div>
-        </div>       
-        <Button severity="warning" @click="$emit('restore')" class="font-bold">Восстановить</Button>
+  <div
+    class="flex items-center justify-between p-4 max-w-full bg-yellow-400 rounded"
+  >
+    <div class="flex items-center">
+      <i class="fas fa-trash mr-2 font-medium text-yellow-800" />
+      <div class="text-yellow-800 font-medium">
+        <slot />
+      </div>
     </div>
+    <Button
+      severity="warning"
+      class="font-bold"
+      @click="$emit('restore')"
+    >
+      Восстановить
+    </Button>
+  </div>
 </template>
