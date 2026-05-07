eb708dde09647d51ab304d0292b5695cd74e9f97
OlegTDev <_alexeevich_@list.ru>
2026-05-07 17:52:18 +0500

    Добавлены проверки в eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,15 @@
 import pluginVue from 'eslint-plugin-vue';
+import js from '@eslint/js';
+
 export default [
+  js.configs.recommended,
   {
     ignores: ['storage/**', 'node_modules/**', 'dist/**'],
   },
   ...pluginVue.configs['flat/recommended'],
   {
     rules: {
+      'semi': ['warn', 'always'],
       'vue/multi-word-component-names': 'off',
       'vue/require-default-prop': 'off',
       'vue/max-attributes-per-line': [
