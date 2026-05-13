c53dc0c99c9f55317b47a0c8ce7b064731766b8e
OlegTDev <_alexeevich_@list.ru>
2026-05-13 14:37:04 +0500

    Рефакторинг Users\*

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -1,91 +1,69 @@
 <script setup>
-import { Head, useForm } from '@inertiajs/vue3'
-import Layout from '@/Shared/Layout'
-import LoadingButton from '@/Shared/LoadingButton'
-import Breadcrumbs from '@/Shared/Breadcrumbs'
-import Panel from 'primevue/panel'
-import Label from '@/Shared/Label'
-import InputText from 'primevue/inputtext'
-import InlineMessage from 'primevue/inlinemessage'
-import { inject } from 'vue'
+import { Head, useForm } from '@inertiajs/vue3';
+import Layout from '@/Shared/Layout';
+import LoadingButton from '@/Shared/LoadingButton';
+import Breadcrumbs from '@/Shared/Breadcrumbs';
+import Panel from 'primevue/panel';
+import Label from '@/Shared/Label';
+import InputText from 'primevue/inputtext';
+import { useConfig } from '@/Composables/useConfig';
+import Message from 'primevue/message';
 
 defineOptions({
-    layout: Layout
-})
+  layout: Layout,
+});
 
-const props = defineProps({
-    domainName: String,
-})
-
-const urls = inject('urls')
+const { urls } = useConfig();
 
 const form = useForm({
-    name: null,
-    domain: props.domainName,
-})
-
-const LogActions = inject('LogActions');
+  name: null,
+});
 
 const save = () => {
-    const url = urls.users.store();
-    form.post(url, {
-        onSuccess: () => {
-            LogActions.save(url, 'POST', 'Добавление пользователя', {
-                name: form.name,
-                domain: form.domain,
-            });
-        },
-    })
-}
-
+  form.post(urls.users.store());
+};
 </script>
-
 <template>
-    <div>
+  <div>
+    <Head title="Добавление пользователя" />
 
-        <Head title="Добавление пользователя" />
+    <Breadcrumbs
+      :home="{ label: 'Главная', url: urls.home }"
+      :items="[
+        { label: 'Пользователи', url: urls.users.index() },
+        { label: form.name }
+      ]"
+    />
 
-        <Breadcrumbs :home="{ label: 'Главная', url: urls.home }" :items="[
-            { label: 'Пользователи', url: urls.users.index() },
-            { label: form.name }
-        ]" />
-
-        <form @submit.prevent="save">
-            <Panel>
-                <template #header>
-                    <h1 class="font-bold text-xl">Добавление пользователя</h1>
-                </template>
-                    <div class="max-w-2xl">
-                        <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                            <div class="sm:col-span-4">
-                                <Label for="domain">Домен</Label>
-                                <InputText
-                                    class="w-full"
-                                    v-model="form.domain"
-                                    placeholder="DOMAIN"
-                                    :invalid="form.errors?.domain?.length > 0"
-                                />
-                                <InlineMessage v-if="form.errors?.domain" class="mt-2" severity="error">{{ form.errors?.domain }}</InlineMessage>
-                            </div>
-                        </div>
-                        <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                            <div class="sm:col-span-4">
-                                <Label for="name">Учетная запись</Label>
-                                <InputText
-                                    class="w-full"
-                                    v-model="form.name"
-                                    placeholder="XXXX-XX-XXX"
-                                    :invalid="form.errors?.name?.length > 0"
-                                />
-                                <InlineMessage v-if="form.errors?.name" class="mt-2" severity="error">{{ form.errors?.name }}</InlineMessage>
-                            </div>
-                        </div>
-                    </div>
-                <template #footer>
-                    <loading-button :loading="form.processing" class="font-bold" type="submit">Сохранить</loading-button>
-                </template>
-            </Panel>
-        </form>
-
-    </div>
+    <form @submit.prevent="save">
+      <Panel>
+        <template #header>
+          <h1 class="font-bold text-xl">
+            Добавление пользователя
+          </h1>
+        </template>
+        <div class="max-w-2xl">
+          <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div class="sm:col-span-4">
+              <Label for="name">Учетная запись</Label>
+              <InputText
+                v-model="form.name"
+                placeholder="XXXX-XX-XXX"
+                class="w-full"
+                :invalid="form.errors?.name?.length > 0"
+              />
+              <Message v-if="form.errors?.name" class="mt-2" severity="error">
+                {{ form.errors?.name }}
+              </Message>
+            </div>
+          </div>
+        </div>
+        <template #footer>
+          <loading-button :loading="form.processing" class="font-bold" type="submit">
+            Сохранить
+          </loading-button>
+        </template>
+      </Panel>
+    </form>
+  </div>
 </template>
--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -1,159 +1,178 @@
 <script setup>
-import { Head, router } from '@inertiajs/vue3'
-import pickBy from 'lodash/pickBy'
-import Layout from '@/Shared/Layout'
-import throttle from 'lodash/throttle'
-import Tag from 'primevue/tag'
-import { inject, reactive, ref, watch } from 'vue'
-import { onMounted } from 'vue'
-import { initFlowbite } from 'flowbite'
-import DataTable from 'primevue/datatable'
-import Column from 'primevue/column'
-import InputText from 'primevue/inputtext'
-import IconField from 'primevue/iconfield'
-import InputIcon from 'primevue/inputicon'
-import MultiSelect from 'primevue/multiselect'
-import Breadcrumbs from '@/Shared/Breadcrumbs'
-import Button from 'primevue/button'
-
+import { Head, router } from '@inertiajs/vue3';
+import pickBy from 'lodash/pickBy';
+import Layout from '@/Shared/Layout';
+import throttle from 'lodash/throttle';
+import Tag from 'primevue/tag';
+import { reactive, ref, watch } from 'vue';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import InputText from 'primevue/inputtext';
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import MultiSelect from 'primevue/multiselect';
+import Breadcrumbs from '@/Shared/Breadcrumbs';
+import Button from 'primevue/button';
+import { useConfig } from '@/Composables/useConfig';
 
 const props = defineProps({
-    filters: Object,
-    users: Array,
-    roles: Array,
-})
+  filters: Object,
+  users: Array,
+  roles: Array,
+});
 
 defineOptions({
-    layout: Layout
-})
-const filters = reactive(props.filters)
+  layout: Layout
+});
 
-const urls = inject('urls')
+const filters = reactive(props.filters);
+
+const { urls } = useConfig();
 
 const form = reactive({
-    search: filters.search,
-    role: filters.role,
-    trashed: filters.trashed,
-})
+  search: filters.search,
+  role: filters.role,
+  trashed: filters.trashed,
+});
 
 watch(
-    () => form,
-    throttle(() => {
-        router.get(urls.users.index(), pickBy(form), { preserveState: true })
-    }, 150),
-    { deep: true }
-)
+  () => form,
+  throttle(() => {
+    router.get(urls.users.index(), pickBy(form), { preserveState: true });
+  }, 150),
+  { deep: true }
+);
 
-onMounted(() => {
-    initFlowbite()
-})
 
 const create = () => {
-    router.get(urls.users.create())
-}
+  router.get(urls.users.create());
+};
 
 const onRowSelect = (event) => {
-    router.get(urls.users.edit(event.data.id))
-}
+  router.get(urls.users.edit(event.data.id));
+};
 
 const refTableUsers = ref(null);
 
 const onPageChange = () => {
-    const elementTableUsers = refTableUsers.value.$el;
-    if (elementTableUsers) {
-        elementTableUsers.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
-}
+  const elementTableUsers = refTableUsers.value.$el;
+  if (elementTableUsers) {
+    elementTableUsers.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+};
 
-const title = 'Пользователи'
+const title = 'Пользователи';
 
 </script>
 <template>
-    <div>
-        <Head :title="title" />
+  <div>
+    <Head :title="title" />
 
-        <Breadcrumbs :home="{ label: 'Главная', url: urls.home }" :items="[
-            { label: title },
-        ]" />
+    <Breadcrumbs
+      :home="{ label: 'Главная', url: urls.home }"
+      :items="[
+        { label: title },
+      ]"
+    />
 
-        <div class="flex justify-stretch bg-white rounded-md shadow overflow-hidden mt-4">
-            <DataTable
-                ref="refTableUsers"
-                :value="users"
-                paginator :rows="10"
-                dataKey="id" :metaKeySelection="false"
-                class="w-full" tableStyle="min-width: 50rem" selectionMode="single"
-                @rowSelect="onRowSelect"
-                @page="onPageChange"
-            >
-                <template #header>
-                    <div class="flex justify-between">
-                        <Button @click="create">Добавить</Button>
-                        <div class="flex">
-                            <MultiSelect v-model="form.role" :options="roles" optionValue="name" optionLabel="description" placeholder="Роли" class="w-56" />
-                            <IconField iconPosition="left" class="ml-3">
-                                <InputIcon><i class="pi pi-search"></i></InputIcon>
-                                <InputText v-model="form.search" placeholder="Поиск"></InputText>
-                            </IconField>
-                        </div>
-                    </div>
-                </template>
+    <div class="flex justify-stretch bg-white rounded-md shadow overflow-hidden mt-4">
+      <DataTable
+        ref="refTableUsers"
+        :value="users"
+        paginator
+        :rows="10"
+        data-key="id"
+        :meta-key-selection="false"
+        class="w-full"
+        table-style="min-width: 50rem"
+        selection-mode="single"
+        @row-select="onRowSelect"
+        @page="onPageChange"
+      >
+        <template #header>
+          <div class="flex justify-between">
+            <Button @click="create">
+              Добавить
+            </Button>
+            <div class="flex">
+              <MultiSelect
+                v-model="form.role"
+                :options="roles"
+                option-value="name"
+                option-label="description"
+                placeholder="Роли"
+                class="w-56"
+              />
+              <IconField icon-position="left" class="ml-3">
+                <InputIcon><i class="pi pi-search" /></InputIcon>
+                <InputText v-model="form.search" placeholder="Поиск" />
+              </IconField>
+            </div>
+          </div>
+        </template>
 
-                <Column field="org_code" header="Код НО" />
-                <Column field="name" header="Учетная запись">
-                    <template #body="{ data }">
-                        <div class="flex items-center">
-                            <img v-if="data.photo" class="block -my-2 mr-2 w-5 h-5 rounded-full" :src="data.photo" />
-                            {{ data.name }}
-                        </div>
-                    </template>
-                </Column>
-                <Column field="fio" header="ФИО" />
-                <Column field="department" header="Отдел" />
-                <Column field="lotus_mail" header="Коммуникация" class="w-50">
-                    <template #body="{ data }">
-                        <div v-if="data.telephone">
-                            <span class="pi pi-phone me-1"></span>
-                            {{ data.telephone }}
-                        </div>
-                        <div v-if="data.lotus_mail">
-                            <span class="pi pi-send me-1 mt-2"></span>
-                            {{ data.lotus_mail }}
-                        </div>
-                    </template>
-                </Column>
-                <Column header="Роли" class="w-64">
-                    <template #body="{ data }">
-                        <ul v-if="data.roles?.length > 0">
-                            <li v-for="role in data.roles"
-                                class="bg-blue-100 text-blue-800 text-sm font-medium m-2 px-2.5 py-0.5 rounded-lg">
-                                {{ role.description }}
-                            </li>
-                        </ul>
-                        <span v-else class="bg-yellow-100 text-yellow-800 text-sm font-medium m-2 px-2.5 py-0.5 rounded-lg">
-                            Нет ролей
-                        </span>
-                    </template>
-                </Column>
-                <Column header="Контекст" class="w-52">
-                    <template #body="{ data }">
-                        <ul v-if="data.organizations.length > 0">
-                            <li v-for="organization in data.organizations" class="bg-blue-100 text-blue-800 text-sm font-medium m-2 px-2.5 py-0.5 rounded-lg">
-                                {{ organization.name }}
-                            </li>
-                        </ul>
-                        <span v-else class="bg-yellow-100 text-yellow-800 text-sm font-medium m-2 px-2.5 py-0.5 rounded-lg">
-                            Без контекста
-                        </span>
-                    </template>
-                </Column>
-                <Column header="Статус">
-                    <template #body="{ data }">
-                        <Tag v-if="data.deleted_at" severity="danger" icon="pi pi-times" value="Удалена" />
-                        <Tag v-else severity="success" icon="pi pi-check" value="Действующая" />
-                    </template>
-                </Column>
-            </DataTable>
-        </div>
+        <Column field="org_code" header="Код НО" />
+        <Column field="name" header="Учетная запись">
+          <template #body="{ data }">
+            <div class="flex items-center">
+              <img v-if="data.photo" class="block -my-2 mr-2 w-5 h-5 rounded-full" :src="data.photo">
+              {{ data.name }}
+            </div>
+          </template>
+        </Column>
+        <Column field="fio" header="ФИО" />
+        <Column field="department" header="Отдел" />
+        <Column field="lotus_mail" header="Коммуникация" class="w-50">
+          <template #body="{ data }">
+            <div v-if="data.telephone">
+              <i class="pi pi-phone me-1" />
+              {{ data.telephone }}
+            </div>
+            <div v-if="data.lotus_mail">
+              <i class="pi pi-send me-1 mt-2" />
+              {{ data.lotus_mail }}
+            </div>
+          </template>
+        </Column>
+        <Column header="Роли" class="w-64">
+          <template #body="{ data }">
+            <div v-if="data.roles?.length > 0" class="grid grid-cols-1 gap-2 text-sm font-medium">
+              <div
+                v-for="role in data.roles"
+                :key="role.name"
+                class="bg-blue-100 text-blue-800 px-2.5 py-0.5 rounded text-center"
+              >
+                {{ role.description }}
+              </div>
+            </div>
+            <span v-else class="bg-yellow-100 text-yellow-800 px-2.5 py-0.5 rounded">
+              Нет ролей
+            </span>
+          </template>
+        </Column>
+        <Column header="Контекст" class="w-52">
+          <template #body="{ data }">
+            <div v-if="data.organizations.length > 0" class="grid grid-cols-1 gap-2 text-sm font-medium">
+              <div
+                v-for="organization in data.organizations"
+                :key="organization.code"
+                class="bg-blue-100 text-blue-800 px-2.5 py-0.5 rounded"
+              >
+                {{ organization.name }}
+              </div>
+            </div>
+            <span v-else class="bg-yellow-100 text-yellow-800 px-2.5 py-0.5 rounded">
+              Без контекста
+            </span>
+          </template>
+        </Column>
+        <Column header="Статус">
+          <template #body="{ data }">
+            <Tag v-if="data.deleted_at" severity="danger" icon="pi pi-times" value="Удалена" />
+            <Tag v-else severity="success" icon="pi pi-check" value="Действующая" />
+          </template>
+        </Column>
+      </DataTable>
     </div>
+  </div>
 </template>
