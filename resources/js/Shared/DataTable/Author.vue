1145b0d0993e7eeb7463c60dba09555f28eacb36
OlegTDev <_alexeevich_@list.ru>
2026-05-14 13:59:12 +0500

    Доработка общего компонента Author

--- resources/js/Shared/DataTable/Author.vue
+++ resources/js/Shared/DataTable/Author.vue
@@ -1,19 +1,27 @@
 <script setup>
 defineProps({
-    login: String,
-    fullName: String,
-    post: String,
-    department: String,
+  user: {
+    type: Object,
+    required: true,
+  }
 })
 </script>
 <template>
-    <div>
-        {{ login }}
-    </div>
-    <div v-if="fullName">
-        {{ fullName }}
-    </div>
-    <div v-if="post || department">
-        {{ post }} - {{ department }}
-    </div>
+  <div class="grid gap-y-1">
+    <span class="font-medium">
+      {{ user.fio }}
+      <span v-if="user.name">
+        ({{ user.name }})
+      </span>
+    </span>
+    <span v-if="user.post" class="text-xs text-gray-500">
+      {{ user.post }}
+    </span>
+    <span v-if="user.department" class="text-xs text-gray-500">
+      {{ user.department }}
+    </span>
+    <span v-if="user.telephone" class="text-xs text-gray-500">
+      {{ user.telephone }}
+    </span>
+  </div>
 </template>
