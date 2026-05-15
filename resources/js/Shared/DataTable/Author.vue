97005f580b7a197fee2c77ef6ebead00d6df73cd
OlegTDev <_alexeevich_@list.ru>
2026-05-15 15:19:16 +0500

    Доработан компонент Author: вынесены классы в пропсы

--- resources/js/Shared/DataTable/Author.vue
+++ resources/js/Shared/DataTable/Author.vue
@@ -3,24 +3,32 @@ defineProps({
   user: {
     type: Object,
     required: true,
-  }
-})
+  },
+  classMain: {
+    type: String,
+    default: 'font-medium',
+  },
+  classExtra: {
+    type: String,
+    default: 'text-xs text-gray-500',
+  },
+});
 </script>
 <template>
   <div class="grid gap-y-1">
-    <span class="font-medium">
+    <span :class="classMain">
       {{ user.fio }}
       <span v-if="user.name">
         ({{ user.name }})
       </span>
     </span>
-    <span v-if="user.post" class="text-xs text-gray-500">
+    <span v-if="user.post" :class="classExtra">
       {{ user.post }}
     </span>
-    <span v-if="user.department" class="text-xs text-gray-500">
+    <span v-if="user.department" :class="classExtra">
       {{ user.department }}
     </span>
-    <span v-if="user.telephone" class="text-xs text-gray-500">
+    <span v-if="user.telephone" :class="classExtra">
       {{ user.telephone }}
     </span>
   </div>
