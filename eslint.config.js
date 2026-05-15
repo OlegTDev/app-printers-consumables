a6a529827fdb71d552ff87a08d52e00359e5e450
OlegTDev <_alexeevich_@list.ru>
2026-05-15 16:24:59 +0500

    Установлен eslint-plugin-import для проверки расширения vue

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,24 @@
 import pluginVue from 'eslint-plugin-vue';
 import js from '@eslint/js';
+import pluginImport from 'eslint-plugin-import';
 
 export default [
   js.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
   {
+    plugins: {
+      import: pluginImport,
+    },
+    settings: {
+      'import/resolver': {
+        alias: {
+          map: [
+            ['@', './resources/js']
+          ],
+          extensions: ['.js', '.vue']
+        }
+      }
+    },
     rules: {
       'semi': ['warn', 'always'],
       'vue/multi-word-component-names': 'off',
@@ -12,13 +26,19 @@ export default [
       'vue/max-attributes-per-line': [
         'error',
         {
-          singleline: {
-            max: 5,
-          },
-          multiline: {
-            max: 1,
-          },
+          singleline: { max: 5 },
+          multiline: { max: 1 },
         },
+      ],
+      'import/extensions': [
+        'warn',
+        'always',
+        {
+          js: 'never',
+          ts: 'never',
+          mjs: 'never',
+          vue: 'always',
+        }
       ],
     },
   },
