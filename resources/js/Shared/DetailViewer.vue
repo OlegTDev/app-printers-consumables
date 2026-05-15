f78e83af99d361523cf02d910cf8e797d1a9f8d0
OlegTDev <_alexeevich_@list.ru>
2026-05-15 15:17:44 +0500

    Добавлены слоты в компонент DetailViewer

--- resources/js/Shared/DetailViewer.vue
+++ resources/js/Shared/DetailViewer.vue
@@ -21,33 +21,41 @@ const { fromNow, formatDate } = useDate();
 </script>
 <template>
   <table
-    class="w-1/2 text-left text-gray-700"
+    class="lg:w-2/3 w-full text-left text-gray-700"
     :class="classRow"
   >
-    <tr
-      v-for="item in items"
-      :key="item.value"
-      class="bg-white border-b border-b-gray-200"
-      :class="$attrs.classRow"
-    >
-      <th
-        scope="row"
-        class="px-6 py-4"
-        :class="item?.classTh"
-      >
-        {{ item.label }}
-      </th>
+    <tbody>
+      <template v-for="item in items" :key="item.value">
+        <tr
+          v-if="!item.hide"
+          class="bg-white border-b border-b-gray-200"
+          :class="$attrs.classRow"
+        >
+          <th
+            scope="row"
+            class="px-6 py-4"
+            :class="item?.classTh"
+          >
+            {{ item.label }}
+          </th>
 
-      <td class="px-6 py-4" :class="item?.classTd">
-        <template v-if="item.value && (item?.is_date ?? false)">
-          <i v-if="item?.icon" :class="item.icon" />
-          {{ fromNow(item.value) }}
-          ({{ formatDate(item.value) }})
-        </template>
-        <template v-else>
-          {{ item.value }}
-        </template>
-      </td>
-    </tr>
+          <td class="px-6 py-4" :class="item?.classTd">
+            <template v-if="item.keySlot">
+              <slot :name="item.keySlot" />
+            </template>
+            <template v-else>
+              <template v-if="item.value && (item?.is_date ?? false)">
+                <i v-if="item?.icon" :class="item.icon" />
+                {{ fromNow(item.value) }}
+                ({{ formatDate(item.value) }})
+              </template>
+              <template v-else>
+                {{ item.value }}
+              </template>
+            </template>
+          </td>
+        </tr>
+      </template>
+    </tbody>
   </table>
 </template>
