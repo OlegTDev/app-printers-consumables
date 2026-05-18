6f10c77c10e35d11f9d7ae3f2c8a00eb4b021e4d
OlegTDev <_alexeevich_@list.ru>
2026-05-18 16:37:12 +0500

    Добавлена точка с запятой в app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,7 +21,7 @@ import { config } from '@/config/config';
 import { urls } from '@/config/urls';
 import { Auth } from '@/auth';
 import { LogActions } from './logActions';
-import { ru } from 'primelocale/js/ru.js'
+import { ru } from 'primelocale/js/ru.js';
 
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
