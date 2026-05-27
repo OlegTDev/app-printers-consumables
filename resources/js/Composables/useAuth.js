c74573401a4cacf3ab01ce4c31dcf4ba56b8702e
OlegTDev <_alexeevich_@list.ru>
2026-05-27 17:14:17 +0500

    В useAuth добавлены типы jsdoc для более удобного использования

--- resources/js/Composables/useAuth.js
+++ resources/js/Composables/useAuth.js
@@ -1,10 +1,25 @@
 import { usePage } from "@inertiajs/vue3";
 import { computed } from "vue";
 
+/**
+ * @returns {{
+ *  user: import('vue').ComputedRef<{
+ *    id: number;
+ *    name: string;
+ *    fio: string;
+ *    org_code: string;
+ *    email: string;
+ *    roles: string[];
+ *  } | null>,
+ *  isAdmin: import('vue').ComputedRef<boolean>,
+ *  userRoles: import('vue').ComputedRef<string[]>,
+ *  can: (...roles: string[]) => boolean,
+ * }}
+ */
 export function useAuth() {
   const page = usePage();
   const user = computed(() => page.props.auth?.user);
-  const userRoles = computed(() =>page.props.auth?.user?.roles ?? []);
+  const userRoles = computed(() => page.props.auth?.user?.roles ?? []);
   const isAdmin = computed(() => userRoles.value.includes('admin'));
 
   const can = (...roles) => {
