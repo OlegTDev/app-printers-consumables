ef5abc23ee4de04d98f9ea06e8c9c18373b88546
OlegTDev <_alexeevich_@list.ru>
2026-05-27 17:15:35 +0500

    Установлены пакеты и плагины для с большим паком иконок

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,39 @@ import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite';
+import Components from 'unplugin-vue-components/vite';
+import IconsResolver from 'unplugin-icons/resolver';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    inertia(),
+    tailwindcss(),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+    Components({
+      resolvers: [
+        IconsResolver({
+          prefix: 'i',
         }),
-        inertia(),
-        tailwindcss(),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    resolve: {
-        extensions: ['.js', '.vue', '.json', '.ts'],
-    },
+      ],
+    }),
+    Icons({
+      compiler: 'vue3',
+      autoInstall: true,
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.vue', '.json', '.ts'],
+  },
 });
