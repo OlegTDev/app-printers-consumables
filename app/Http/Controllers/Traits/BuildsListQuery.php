0f5e4f8b6a555b6e45c389b093da392e98349441
OlegTDev <_alexeevich_@list.ru>
2026-05-29 15:43:28 +0500

    Добавлен трейт BuildsListQuery для фильтрации, сортироки, пагинации

---
+++ app/Http/Controllers/Traits/BuildsListQuery.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Traits;
+
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder as BuilderEloquent;
+use Illuminate\Pagination\AbstractPaginator;
+
+trait BuildsListQuery
+{
+    private const string SORT_FIELD = 'sortField';
+    private const string SORT_ORDER = 'sortOrder';
+
+    public function paginate(
+        Request $request,
+        BuilderEloquent $query,
+        array $allowSortFields = [],
+        array $filterFields = ['search'],
+        int $perPage = 10,
+    ): AbstractPaginator {
+        return $this->baseBuildQuery($request, $query, $allowSortFields, $filterFields, $perPage);
+    }
+
+    public function getPaginatedData(
+        Request $request,
+        BuilderEloquent $query,
+        array $allowSortFields = [],
+        array $filterFields = ['search'],
+        int $perPage = 10,
+    ): array {
+        return [
+            'items' => $this->baseBuildQuery($request, $query, $allowSortFields, $filterFields, $perPage),
+            'query' => $request->only([...$filterFields, self::SORT_FIELD, self::SORT_ORDER]),
+        ];
+    }
+
+    private function baseBuildQuery(
+        Request $request,
+        BuilderEloquent $query,
+        array $allowSortFields = [],
+        array $filterFields = ['search'],
+        int $perPage = 10,
+    ): AbstractPaginator
+    {
+        $queryClone = clone $query;
+
+        $this->applyFilter($request, $queryClone, $filterFields);
+        $this->applySort($request, $queryClone, $allowSortFields);
+
+        return $queryClone->paginate($perPage)->withQueryString();
+    }
+
+    /**
+     * @param Request $request
+     * @param BuilderEloquent $buildQuery
+     * @param array $fields
+     */
+    private function applyFilter(Request $request, BuilderEloquent $buildQuery, array $fields): void
+    {
+        $model = $buildQuery->getModel();
+        if (method_exists($model, 'scopeFilter')) {
+            // @phpstan-ignore method.notFound
+            $buildQuery->filter($request->only($fields));
+        }
+    }
+
+    private function applySort(Request $request, BuilderEloquent $buildQuery, ?array $allowedFields = null): void
+    {
+        $allowedFields ??= $buildQuery->getModel()->getFillable();
+
+        $field = $request->input(self::SORT_FIELD);
+        $order = $request->input(self::SORT_ORDER, '1') == '1' ? 'asc' : 'desc';
+
+        if ($field && in_array($field, $allowedFields)) {
+            $buildQuery->reorder()->orderBy($field, $order);
+        }
+    }
+}
