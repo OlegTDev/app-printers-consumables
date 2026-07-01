b54e26a6ae89810b48738b205425734cba0bdf66
OlegTDev <_alexeevich_@list.ru>
2026-07-01 16:57:22 +0500

    Добавлен сервис ManufacturerQueryService

--- app/Models/Manufacturer.php
+++ app/Models/Manufacturer.php
@@ -5,6 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property mixed $created_at
+ * @property mixed $updated_at
+ */
 class Manufacturer extends Model
 {
     use HasFactory;
