3e22e2de256d65e448b2e2d815105185755f07ad
OlegTDev <_alexeevich_@list.ru>
2026-07-02 14:21:35 +0500

    Доработан сервис OrganizationQueryService. Добавлены функции для построения дерева организаций

--- app/Services/Query/OrganizationQueryService.php
+++ app/Services/Query/OrganizationQueryService.php
@@ -14,7 +14,7 @@ class OrganizationQueryService
             $query->join('users_organizations', 'users_organizations.org_code', '=', 'organizations.code')
                 ->where('users_organizations.id_user', $userId);
         }
-        return $query->pluck('code')->toArray();
+        return $query->pluck('organizations.code')->toArray();
     }
 
     public function getOrganizationsByCodes(array $availableCodes): array
@@ -23,6 +23,65 @@ class OrganizationQueryService
             ->whereIn('code', $availableCodes)
             ->get()
             ->toArray();
+    }
+
+    public function getAllOrganizationsQuery()
+    {
+        return Organization::query();
+    }
+
+    public function getOrganizationsTree(array $items)
+    {
+        $flatContainer = [];
+        $tree = [];
+
+        foreach ($items as $item) {
+            $flatContainer[$item['code']] = $this->treeItemMapper($item);
+            $flatContainer[$item['code']]['children'] = [];
+        }
+
+        foreach ($flatContainer as $code => &$node) {
+            $parentCode = $node['data']['parent'];
+
+            if ($parentCode === null) {
+                $tree[$code] = &$node;
+            } else {
+                if (isset($flatContainer[$parentCode])) {
+                    $flatContainer[$parentCode]['children'][$code] = &$node;
+                } else {
+                    $tree[$code] = &$node;
+                }
+            }
+        }
+
+        return $this->cleanKeys($tree);
+    }
+
+    private function treeItemMapper($item)
+    {
+        return [
+            'key' => $item['code'],
+            'data' => [
+                'code' => $item['code'],
+                'parent' => $item['parent'],
+                'name' => $item['name'],
+                'created_at' => $item['created_at'],
+                'updated_at' => $item['updated_at'],
+            ],
+        ];
+    }
+
+    private function cleanKeys(array $tree): array
+    {
+        $result = array_values($tree);
+        foreach ($result as $key => $item) {
+            if (isset($item['children']) && !empty($item['children'])) {
+                $result[$key]['children'] = $this->cleanKeys($item['children']);
+            } else {
+                $result[$key]['children'] = [];
+            }
+        }
+        return $result;
     }
 
 }
