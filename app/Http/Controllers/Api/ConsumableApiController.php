036db61a45df09a6c35ab5254a9741ffdcf28741
OlegTDev <_alexeevich_@list.ru>
2026-07-08 12:56:23 +0500

    ConsumableApiController вынесена функция lastConsumableCountInstalled из контроллера ConsumablesCountsInstalledController

--- app/Http/Controllers/Api/ConsumableApiController.php
+++ app/Http/Controllers/Api/ConsumableApiController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Resources\ConsumableCountInstalledResource;
 use App\Http\Resources\ConsumableResource;
+use App\Models\Consumable\CartridgeColors;
 use App\Models\Consumable\Consumable;
+use App\Models\Consumable\ConsumableCountInstalled;
+use App\Models\Consumable\ConsumableTypesEnum;
 use App\Models\Printer\Printer;
+use Illuminate\Database\Eloquent\Builder;
 
 class ConsumableApiController
 {
@@ -24,5 +29,26 @@ class ConsumableApiController
     {
         $items = Consumable::queryWithoutOtherTypesByPrinter()->get();
         return ConsumableResource::collection($items);
+    }
+
+    /**
+     * @route GET /consumables/counts/installed/last
+     */
+    public function lastConsumableCountInstalled(): array
+    {
+        $limit = config('per_page.consumable_last_installed', 30);
+
+        $query = ConsumableCountInstalled::with(['consumableCount.consumable', 'printerWorkplace.printer', 'author'])
+            ->whereHas('printerWorkplace', fn(Builder $query) => $query->where('org_code', auth()->user()->org_code))
+            ->orderByDesc('created_at')
+            ->limit($limit);
+
+        $data = ConsumableCountInstalledResource::collection($query->get());
+
+        return [
+            'data' => $data,
+            'cartridgeColors' => CartridgeColors::get(),
+            'consumableTypes' => ConsumableTypesEnum::array(),
+        ];
     }
 }
