a04a9250eaccd1dfa84dfd548265483079e30dc0
OlegTDev <_alexeevich_@list.ru>
2026-07-08 15:39:20 +0500

    Рефакторинг и облегчение моделей ConsumableCountAdded и ConsumableCountInstalled. Корректировка сигнатуры функции в ConsumableTypesEnum

--- app/Models/Consumable/ConsumableCountInstalled.php
+++ app/Models/Consumable/ConsumableCountInstalled.php
@@ -6,11 +6,11 @@ use App\Models\Auth\User;
 use App\Models\Printer\PrinterWorkplace;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * Запись об установке расходного материала
- * 
+ *
  * @property int $id
  * @property int $id_consumable_count
  * @property int $id_printer_workplace
@@ -18,10 +18,10 @@ use Illuminate\Support\Facades\Auth;
  * @property int $count
  * @property string $created_at
  * @property string $updated_at
- * 
- * @property ConsumableCount $consumableCount
- * @property PrinterWorkplace $printerWorkplace
- * @property User $author
+ *
+ * @property-read ConsumableCount $consumableCount
+ * @property-read PrinterWorkplace $printerWorkplace
+ * @property-read User $author
  */
 class ConsumableCountInstalled extends Model
 {
@@ -32,84 +32,19 @@ class ConsumableCountInstalled extends Model
      */
     protected $table = 'consumables_counts_installed';
 
-    /**
-     * {@inheritDoc}
-     * 
-     * Сохранение данных о количестве в родительской записи 
-     * с общим количеством расходных материалов
-     * При добавлении уменьшается текущее количество к общему количеству в родительской записи
-     * При удалении прибавляется текущее количество от общего количества в родительской записи
-     * @param self $model текущий объект
-     * @param bool $isPlus (true - если увеличивается, false - если уменьшается) 
-     */
-    private static function updateModel(self $model, bool $isPlus = true)
-    {                
-        $consumableCount = $model->consumableCount;
-        if ($consumableCount === null) {
-            throw new \Exception("Parent record ConsumableCount with id={$model->id_consumable_count} not found!");
-        }
-        $count = $model->count;
-        if (!$isPlus) {
-            $count = $count * -1;
-        }
-        $consumableCount->count += $count;
-        $consumableCount->save();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public static function booting()
-    {
-        parent::booting();
-         // создание объекта
-        static::creating(function(self $model) {
-            $model->id_author = Auth::user()->id;            
-            self::updateModel($model, false);
-        });
-        // удаление объекта
-        static::deleted(function(self $model) {          
-            self::updateModel($model, true);
-        });
-    }
-
-    /**
-     * Родительская запись с общим количеством расходного материала
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function consumableCount()
+    public function consumableCount(): BelongsTo
     {
         return $this->belongsTo(ConsumableCount::class, 'id_consumable_count');
     }
 
-    /**
-     * Автор записи
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function author()
+    public function author(): BelongsTo
     {
         return $this->belongsTo(User::class, 'id_author');
     }
 
-    /**
-     * Принтер на рабочем месте
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function printerWorkplace()
+    public function printerWorkplace(): BelongsTo
     {
         return $this->belongsTo(PrinterWorkplace::class, 'id_printer_workplace');
     }
 
-    /**
-     * Описание атрибутов
-     * @return array
-     */
-    public static function labels(): array
-    {
-        return [
-            'id_printer_workplace' => 'Принтер',
-            'count' => 'Количество',
-        ];
-    }
-    
 }
