a0e21472894c584b2107c8d1950e70bdf465e378
OlegTDev <_alexeevich_@list.ru>
2026-07-08 15:40:01 +0500

    Labels для PrinterWorkplace

---
+++ config/labels/printer_workplace.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'id_printer' => 'Принтер',
+    'org_code' => 'Организация',
+    'location' => 'Кабинет',
+    'serial_number' => 'Серийный номер',
+    'inventory_number' => 'Инвентарный номер',
+    'date' => 'Дата',
+    'created_at' => 'Дата создания',
+    'updated_at' => 'Дата изменения',
+    'author' => 'Автор',
+];
