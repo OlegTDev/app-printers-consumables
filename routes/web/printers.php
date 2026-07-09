83ca41cba868f9ffe29b5defa780d9ea12bd062a
OlegTDev <_alexeevich_@list.ru>
2026-07-09 15:17:02 +0500

    Исправлен порядок роутов из за ошибки в printers/workplace/show

--- routes/web/printers.php
+++ routes/web/printers.php
@@ -1,21 +1,23 @@
 <?php
 
+use App\Http\Controllers\Api\ConsumableApiController;
 use App\Http\Controllers\Api\PrintersWorkplaceApiController;
 use App\Http\Controllers\PrintersWorkplaceController;
 
 
 Route::get('printers/workplace/all', [PrintersWorkplaceApiController::class, 'all'])->name('workplace.all');
 
-Route::resource('printers/workplace', PrintersWorkplaceController::class)->only(['index', 'show']);
-
 Route::middleware('role:admin,editor-printer-workplace')->group(function () {
     Route::resource('printers/workplace', PrintersWorkplaceController::class)->only(['create', 'store', 'edit', 'update', 'destroy']);
 });
+
+Route::resource('printers/workplace', PrintersWorkplaceController::class)->only(['index', 'show']);
+
 
 Route::middleware('role:admin,subtract-consumable')->group(function () {
     Route::get('printers/workplace/list/{consumable}', [PrintersWorkplaceApiController::class, 'listByConsumable'])
         ->name('printers-workplace.list');
 });
 
-Route::get('printers/workplace/consumables-installed/{workplace}', [PrintersWorkplaceController::class, 'consumablesInstalled'])
+Route::get('printers/workplace/consumables-installed/{workplace}', [PrintersWorkplaceApiController::class, 'consumablesInstalledByPrinterWorkplace'])
     ->name('printers.workplace.consumables-installed');
