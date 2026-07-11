c924bf0ed0646bbfd0dada7882eb2b34c0527b6b
OlegTDev <_alexeevich_@list.ru>
2026-07-11 11:50:09 +0500

    В роуты users вынесена проверка ролей

--- routes/web/users.php
+++ routes/web/users.php
@@ -3,13 +3,23 @@
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\UsersOrganizationsController;
 
-Route::resource('users', UsersController::class)->except(['show'])->withTrashed(['edit']);
-Route::put('users/{user}/restore', [UsersController::class, 'restore'])
-    ->name('users.restore')
-    ->withTrashed()
-    ->middleware('role:admin');
 
 Route::get('users/organizations', [UsersOrganizationsController::class, 'index'])
     ->name('users.organizations');
+
 Route::post('users/organizations/{organization}', [UsersOrganizationsController::class, 'change'])
     ->name('users.organizations.change');
+
+Route::resource('users', UsersController::class)
+    ->only(['edit'])
+    ->withTrashed(['edit']);
+
+Route::middleware('role:admin')->group(function () {
+    Route::resource('users', UsersController::class)
+        ->only(['index', 'create', 'store', 'update', 'destroy'])
+        ->withTrashed();
+
+    Route::put('users/{user}/restore', [UsersController::class, 'restore'])
+        ->name('users.restore')
+        ->withTrashed();
+});
