933f7967076aea2676f4970ce43fb089de0293f5
OlegTDev <_alexeevich_@list.ru>
2026-07-31 15:19:53 +0500

    test: тестирование класса Organization

--- tests/Unit/Models/OrganizationTest.php
+++ tests/Unit/Models/OrganizationTest.php
@@ -12,9 +12,7 @@ class OrganizationTest extends TestCase
     use RefreshDatabase;
 
 
-    /**
-     * @var Organization[]
-     */
+    /** @var Organization[] */
     private array $organizations = [];
 
 
@@ -23,37 +21,25 @@ class OrganizationTest extends TestCase
         parent::setUp();
 
 
-        // Organizations
-
-        $this->organizations['parent'] = Organization::create([
-            'code' => 'x0000',
-            'name' => 'Parent Org 0000',
-        ]);
-
-        $this->organizations['child'] = Organization::create([
-            'code' => 'x0001',
-            'name' => 'Child Org 0001',
-            'parent' => 'x0000',
-        ]);
-
-        $this->organizations['parent-some'] = Organization::create([
-            'code' => 'x0003',
-            'name' => 'Parent Org 0003',
-        ]);
+        $this->organizations = Organization::factory()->createMany([
+            ['code' => 'x0000', 'name' => 'Parent Org 0000'],
+            ['code' => 'x0001', 'name' => 'Child Org 0001', 'parent' => 'x0000'],
+            ['code' => 'x0003', 'name' => 'Parent Org 0003'],
+        ])->keyBy('code')->all();
     }
 
     public function test_it_can_has_parent_organization(): void
     {
-        $child = $this->organizations['child'];
-        $parent = $this->organizations['parent'];
+        $child = $this->organizations['x0001'];
+        $parent = $this->organizations['x0000'];
 
         $this->assertTrue($child->parentOrganization->is($parent));
     }
 
     public function test_it_can_has_child_organizations(): void
     {
-        $child = $this->organizations['child'];
-        $parent = $this->organizations['parent'];
+        $child = $this->organizations['x0001'];
+        $parent = $this->organizations['x0000'];
 
         $this->assertTrue($parent->childOrganizations->contains($child));
     }
@@ -62,42 +48,40 @@ class OrganizationTest extends TestCase
     {
         // 1 - text parent
         $organizations1 = Organization::query()->filter(['search' => 'parent'])->get();
-        $this->assertTrue(2 === $organizations1->count());
+        $this->assertCount(2, $organizations1);
 
-        $this->assertTrue($organizations1->doesntContain($this->organizations['child']));
+        $this->assertTrue($organizations1->doesntContain($this->organizations['x0001']));
 
-        $this->assertTrue($organizations1->contains($this->organizations['parent']));
-        $this->assertTrue($organizations1->contains($this->organizations['parent-some']));
+        $this->assertTrue($organizations1->contains($this->organizations['x0000']));
+        $this->assertTrue($organizations1->contains($this->organizations['x0003']));
 
         // 2 - code is x0001
         $organizations2 = Organization::query()->filter(['search' => 'x0001'])->get();
-        $this->assertTrue(1 === $organizations2->count());
+        $this->assertCount(1, $organizations2);
 
-        $this->assertTrue($organizations2->contains($this->organizations['child']));
+        $this->assertTrue($organizations2->contains($this->organizations['x0001']));
 
-        $this->assertTrue($organizations2->doesntContain($this->organizations['parent']));
-        $this->assertTrue($organizations2->doesntContain($this->organizations['parent-some']));
+        $this->assertTrue($organizations2->doesntContain($this->organizations['x0000']));
+        $this->assertTrue($organizations2->doesntContain($this->organizations['x0003']));
     }
 
 
     public function test_it_has_many_users(): void
     {
-        // Create users
-        $user1 = User::create([
+        $user1 = User::factory()->create([
             'name' => 'test_user_0001',
             'email' => 'test_user_0001@test.ru',
             'org_code' => '',
         ]);
-        $user2 = User::create([
+        $user2 = User::factory()->create([
             'name' => 'test_user_0002',
             'email' => 'test_user_0002@test.ru',
             'org_code' => '',
         ]);
 
         // Associate
-        $orgParent001 = $this->organizations['parent'];
+        $orgParent001 = $this->organizations['x0000'];
         $orgParent001->users()->attach([$user1->id]);
-
 
         // Tests
         $orgParent001->load('users');
@@ -105,7 +89,7 @@ class OrganizationTest extends TestCase
         $this->assertTrue($orgParent001->users->contains($user1));
         $this->assertTrue($orgParent001->users->doesntContain($user2));
 
-        $this->assertEquals(1, $orgParent001->users->count());
+        $this->assertCount(1, $orgParent001->users);
     }
 
 }
