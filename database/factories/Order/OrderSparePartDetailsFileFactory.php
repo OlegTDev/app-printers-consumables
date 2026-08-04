0ba058fa4e94450a48965a92322044545a102b7f
OlegTDev <_alexeevich_@list.ru>
2026-08-04 16:20:34 +0500

    feat: добавлена фабрика для модели OrderSparePartDetailsFile

---
+++ database/factories/Order/OrderSparePartDetailsFileFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\Order;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<\App\Models\Order\OrderSparePartDetailsFile>
+ */
+class OrderSparePartDetailsFileFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
